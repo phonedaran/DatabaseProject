0c230a5bb4ab5a97c816bd8750067b3e754d0ae6
phonedaran <35362197+phonedaran@users.noreply.github.com>
2019-10-20 16:08:53 +0700

    image product

--- resources/views/products/Productlist.blade.php
+++ resources/views/products/Productlist.blade.php
@@ -133,21 +133,12 @@
     <main role="main">
         <div class="album py-5 bg-light">
             <div class="container">
-
                 <div class="row">
-
-
                     @foreach ($products as $product )
                     <div class="col-md-4">
                             <div class="card mb-4 shadow-sm">
-                                <!--
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
-                                    focusable="false" role="img" aria-label="Placeholder: Thumbnail">
-                                    <title>Placeholder</title>
-                                    <rect width="100%" height="100%" fill="#55595c" />
-                                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                                </svg>-->
-                                <img src='<?php echo $product->imagePath; ?>' width="100%" height="100%"  />
+                                <img src='images/product/<?php echo $product->productName; ?>.jpg' onerror="this.src='images/not.png'" width="100%" height="100%"  />
+                                <!--<img src='<?php echo 'images/gg.png' ?>' width="100%" height="100%"  />-->
 
                                 <div class="card-body">
                                     <h3>{{$product->productName}}</h3>
