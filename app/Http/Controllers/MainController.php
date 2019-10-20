cf9642b92c2e89842c4eca55271a4ad82f2b57b5
thipaporn <35359046+thipaporn@users.noreply.github.com>
2019-10-20 11:15:03 +0700

    login success

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Validator;
+use Auth;
+
+class MainController extends Controller
+{
+    function index()
+    {
+        return view('login');
+    }
+
+    function checklogin(request $request)
+    {
+
+        $name=$request->input('name');
+        $password=$request->input('pass');
+        $token=DB::select('select remember_token from users where employeeNumber=?',[$name]);
+        // $data=DB::select('select employeeNumber from users where employeeNumber=? and password =DES_DECRYPY(?,?)',
+        // [$name,$password,$token]);
+        $data=DB::select('select employeeNumber from users where employeeNumber=? and password =?',[$name,$password]);
+        if (count($data))
+        {
+            return redirect('main/successlogin');
+        }
+        else
+        {
+        //print_r($data);
+        // print_r($request->input());
+        
+        // $this->validate($request, [
+        //     'name'      =>  'requred|name',
+        //     'pass'      =>  'requred|alphanNum|min:3'
+        // ]);
+
+        // $user_data = array(
+        //     'emnumber'      =>  $request->get('name'),
+        //     'password'      =>  $request->get('password')
+        // );
+
+        // if(Auth::attempt($user_data))
+        // {
+        //     return redirect('main/successlogin');
+        // }
+        // else
+        // {
+            return  redirect()->back()->with('errors','Wrong Login Details!');
+        }
+    }
+
+    function successlogin()
+    {
+        return view('successlogin');
+    }
+
+    function logout()
+    {
+        Auth::logout();
+        return redirect('main');
+    }
+}
---
+++ resources/views/successlogin.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WELCOME TO KIKKOK SHOP </h1>
+    <!-- @if (isset(Auth::user()->name))
+        <div class="alert alert-danger success-block">
+            <strong>Welcome! {{ Auth::user()->name }}</strong>
+            <br>
+            <a href="{{ url('/main/logout') }}">Logout</a>
+        </div>
+    else
+        <script>window.location = "/main";</script>
+    @endif -->
+</body>
+</html>
