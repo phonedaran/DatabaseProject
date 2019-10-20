92dfdddffcf5cbb8e3468f25985824c4b3ca2d90
lydiawimoo <35342723+lydiawimoo@users.noreply.github.com>
2019-10-20 17:23:58 +0700

    icon user

--- storage/framework/views/b8defcde94a1772e57334c709cb4c0ab44cf37a2.php
+++ storage/framework/views/b8defcde94a1772e57334c709cb4c0ab44cf37a2.php
@@ -18,6 +18,8 @@
     <link href="bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
+        crossorigin="anonymous">
    
     <style>
         .bd-placeholder-img {
@@ -57,10 +59,6 @@
                     </div>
                     <div class="col-4 d-flex justify-content-end align-items-center">
                         <a class="text-muted" href="#">
-                    <!-- <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form> -->
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                         stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24" focusable="false">
                             <title>Search</title>
@@ -68,8 +66,7 @@
                             <path d="M21 21l-5.2-5.2" />
                         </svg>
                         </a>
-                        <!-- class="btn" -->
-                        <!-- <a><img src="img/star-icon.png"></a> -->
+                        <a><span class="fas fa-user" style=" color: aliceblue"></span></a>
                     </div>
                 </div>
             </header>
@@ -83,7 +80,6 @@
             <a class="py-2 d-none d-md-inline-block" href="#" style="color:black">Enterprise</a>
             <a class="py-2 d-none d-md-inline-block" href="#" style="color:black">Support</a>
             <a class="py-2 d-none d-md-inline-block" href="#" style="color:black">Pricing</a>
-            <!-- <a class="py-2 d-none d-md-inline-block" href="#" style="color:black"> -->
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true"
                     aria-expanded="false"  style="color:black">
                     Menu
@@ -94,7 +90,7 @@
                     <a class="dropdown-item" href="#">Key Order</a>
                     <a class="dropdown-item" href="#">Order list</a>
                     <a class="dropdown-item" href="#">Promotion</a>
-                    <a class="dropdown-item" href="#">Log out</a>
+                    <a class="dropdown-item" href="<?php echo e(url('/main/logout')); ?>">Log out</a>
                 </div>
             <!-- </a> -->
         </div>
@@ -116,10 +112,11 @@
                 <div class="row">
 
 
-                    <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                     <div class="col-md-4">
                             <div class="card mb-4 shadow-sm">
-                                <img src='<?php echo $product->imagePath; ?>' width="100%" height="100%"  />
+                                <img src='images/product/<?php echo $product->productName; ?>.jpg' onerror="this.src='images/not.png'" width="100%" height="100%"  />
+                                <!--<img src='<?php echo 'images/gg.png' ?>' width="100%" height="100%"  />-->
 
                                 <div class="card-body">
                                     <h3><?php echo e($product->productName); ?></h3>
